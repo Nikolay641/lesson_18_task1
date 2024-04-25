b4208b751d2c5c1c6cdc606656f5480f9b72cf50
Nikolay Iskorostensky <nikolay11.10.1990@gmail.com>
2024-04-25 21:43:20 +0300

    create main

--- lesson_18_task1/lesson_18_task1.cpp
+++ lesson_18_task1/lesson_18_task1.cpp
@@ -1,10 +1,27 @@
 // lesson_18_task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "Header.h"
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int N = 5;
+    Computer* arr = new Computer[N]
+    {
+        {"HP", 1.2, 6, false, 20'000},
+        {"Aser",3.2, 10, true, 43'000},
+        {"Dell",4.2, 16, false, 80'000},
+        {"Asus",3.2, 10, true, 48'000},
+        {"Apple",5.0, 20, false, 90'000},
+    };
+
+    for (int i = 0; i < N; i++)
+    {
+        arr[i].showInfo();
+    }
+
+
+
 }
 
